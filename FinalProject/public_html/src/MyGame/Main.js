7bc0d3bb5d521c7add6bbee9f17f302e3fe85014
SmirkinDino <344919926@qq.com>
2016-07-29 19:02:18 +0800

    龚楚涵
    更新了index,Main，CameraManager

--- FinalProject/public_html/src/MyGame/Main.js
+++ FinalProject/public_html/src/MyGame/Main.js
@@ -11,3 +11,45 @@ function Main(){
 }
 
 gEngine.Core.inheritPrototype(Main,Scene);
+
+Main.prototype.initialize = function(){
+
+    gManager.CameraManager.initManager();
+
+
+    var camera = new Camera(
+        vec2.fromValues(0,0),
+        50,
+        [20,20,400,400]
+    );
+    gManager.CameraManager.registerCamera(camera,2);
+
+};
+
+Main.prototype.draw = function(){
+
+    while(true){
+        // 得到当前的相机
+        var camera = gManager.CameraManager.nextCamera();
+        if(camera){
+
+            // 渲染物体
+            //
+            // gManager.ObjectsPool.renderAll(camera);
+        }
+        // 这一帧渲染结束
+        else {
+            gManager.CameraManager.moveIndexToHead();
+            return;
+        }
+    }
+};
+
+Main.prototype.update = function(){
+
+    // 更新输入
+
+    // 更新物体池内物体
+
+    // 更新相机
+};
